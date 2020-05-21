a75616aa262847d32a6bc7a68a6dd6172cb5ba8a
Nobuyuki Takahiro <studio.quad9@gmail.com>
2020-05-21 17:02:20 +0900

    simplemde first commit

--- src/MDEdit.js
+++ src/MDEdit.js
@@ -1,29 +1,9 @@
-import React from "react";
-import MDEditor from '@uiw/react-md-editor';
-// import MDEditor, { commands } from '@uiw/react-md-editor';
 
-import './assets/css/style.css'
+import React from "react";
+import MDEdit from "simplemdes";
 
 const MDEdit = () => {
-  const [value, setValue] = React.useState("# Hello!");
-  return (
-    <div className="markdown">
-      <MDEditor
-        value={value}
-        onChange={setValue}
-        height={550}
-        tabSize={4}
-        // commands={[
-        //   commands.bold, commands.hr, commands.italic, commands.divider, 
-        //   commands.codeEdit, commands.codeLive, commands.codePreview, commands.divider,
-        //   commands.fullscreen,
-        // ]}
-      />
-    </div>
-  );
+  return (<textarea id="mde"></textarea>)
 }
 
 export default MDEdit
-
-// <MDEditor.Markdown source={value} />
-// style={{ minHeight: "100vh" }}
