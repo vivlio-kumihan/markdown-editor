789a998d3a1cf84be899600be008bdcc82d4fee9
Nobuyuki Takahiro <studio.quad9@gmail.com>
2020-05-22 11:46:22 +0900

    markdown page basic layout set up

--- src/MDEdit.js
+++ src/MDEdit.js
@@ -1,12 +1,40 @@
 import React, {useState} from "react"
 import SimpleMDE from "react-simplemde-editor"
 
+const toolbar = [
+  {
+    name: "save",
+    action: function customFunction(editor) {
+      alert(editor.value())
+      // here save action
+    },
+    className: "fa fa-save",
+    title: "Save",
+  },
+  "|",
+  "bold",
+  "heading",
+  "|",
+  "quote",
+  "unordered-list",
+  "ordered-list",
+  "|",
+  "link",
+  "image",
+  "|",
+  "preview",
+  "side-by-side",
+  "fullscreen",
+  "|",
+  "guide"
+]
+
 const MDEdit = () => {
   const [markdown, setMarkdown] = useState("")
   return (
     <div id="simple-mde">
     <SimpleMDE  onChange={(e) => setMarkdown(e)}
-                options={{spellChecker: false}}
+                options={{spellChecker: false},{toolbar:toolbar}}
     />
     </div>
   )
