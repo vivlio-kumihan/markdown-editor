9dc4a7d0a3534a88f9e4a9997d40998952e034d9
Nobuyuki Takahiro <studio.quad9@gmail.com>
2020-05-23 10:41:41 +0900

    decide markdown library react-simplemde-editor. setup basic.

--- src/MDEdit.js
+++ src/MDEdit.js
@@ -1,46 +1,24 @@
-import React, {useState} from "react"
+import React, {Component, useState} from "react"
 import SimpleMDE from "react-simplemde-editor"
 
-const toolbar = [
-  {
-    name: "save",
-    action: function customFunction(editor) {
-      alert(editor.value())
-      // here save action
-    },
-    className: "fa fa-save",
-    title: "Save",
-  },
-  "|",
-  "bold",
-  "heading",
-  "|",
-  "quote",
-  "unordered-list",
-  "ordered-list",
-  "|",
-  "link",
-  "image",
-  "|",
-  "preview",
-  "side-by-side",
-  "fullscreen",
-  "|",
-  "guide"
-]
-
-const MDEdit = () => {
-  const [markdown, setMarkdown] = useState("")
-  return (
-    <div id="simple-mde">
-    <SimpleMDE  onChange={(e) => setMarkdown(e)}
-                options={{spellChecker: false},{toolbar:toolbar}}
-    />
-    </div>
-  )
+class MDEdit extends Component {
+  handleChange = (value) => {
+    this.setState({mdeValue: value})
+  }
+  render() {
+    return (
+      <div id="simple-mde">
+        <SimpleMDE  id="react-mde"
+                    label="React simple markdown editor"
+                    onChange={this.handleChange} 
+                    options={{
+                              autofocus: true,
+                              spellChecker: false
+                            }}
+        />
+      </div>
+    )
+  }
 }
 
 export default MDEdit
-
-// OPTIONS | SimpleMDE - Markdown Editor
-// https://developer.aliyun.com/mirror/npm/package/simplemde
